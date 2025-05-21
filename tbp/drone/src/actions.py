7b931c42115197d3fac1b038970d8b50de23aaec
Scott Knudstrup <scottknudstrup@gmail.com>
2025-05-21 11:12:21 -0700

    Moved and updated actions

---
+++ tbp/drone/src/actions.py
@@ -0,0 +1,125 @@
+from numbers import Number
+
+from tbp.monty.frameworks.actions.actions import Action
+from tbp.monty.frameworks.actions.actuator import Actuator
+from tbp.monty.frameworks.environments.embodied_environment import ActionSpace
+
+__all__ = [
+    "Action",
+    "DroneActionSpace",
+    "TakeOff",
+    "Land",
+    "TurnLeft",
+    "TurnRight",
+    "SetYaw",
+    "MoveForward",
+    "MoveBackward",
+    "MoveLeft",
+    "MoveRight",
+    "MoveUp",
+    "MoveDown",
+]
+
+
+class DroneActionSpace(tuple, ActionSpace):
+    """Action space for 2D data environments."""
+
+    def sample(self):
+        return self.rng.choice(self)
+
+
+class TakeOff(Action):
+    def __init__(self, agent_id: str):
+        super().__init__(agent_id=agent_id)
+
+    def act(self, actuator: Actuator):
+        actuator.actuate_takeoff(self)
+
+
+class Land(Action):
+    def __init__(self, agent_id: str):
+        super().__init__(agent_id=agent_id)
+
+    def act(self, actuator: Actuator):
+        actuator.actuate_land(self)
+
+
+class TurnLeft(Action):
+    def __init__(self, agent_id: str, angle: Number):
+        super().__init__(agent_id=agent_id)
+        self.angle = angle
+
+    def act(self, actuator: Actuator):
+        actuator.actuate_turn_left(self)
+
+
+class TurnRight(Action):
+    def __init__(self, agent_id: str, angle: Number):
+        super().__init__(agent_id=agent_id)
+        self.angle = angle
+
+    def act(self, actuator: Actuator):
+        actuator.actuate_turn_right(self)
+
+
+class SetYaw(Action):
+    def __init__(self, agent_id: str, yaw: Number):
+        super().__init__(agent_id=agent_id)
+        self.yaw = yaw
+
+    def act(self, actuator: Actuator):
+        actuator.actuate_set_yaw(self)
+
+
+class MoveForward(Action):
+    def __init__(self, agent_id: str, distance: Number):
+        super().__init__(agent_id=agent_id)
+        self.distance = distance
+
+    def act(self, actuator: Actuator):
+        actuator.actuate_move_forward(self)
+
+
+class MoveBackward(Action):
+    def __init__(self, agent_id: str, distance: Number):
+        super().__init__(agent_id=agent_id)
+        self.distance = distance
+
+    def act(self, actuator: Actuator):
+        actuator.actuate_move_backward(self)
+
+
+class MoveLeft(Action):
+    def __init__(self, agent_id: str, distance: Number):
+        super().__init__(agent_id=agent_id)
+        self.distance = distance
+
+    def act(self, actuator: Actuator):
+        actuator.actuate_move_left(self)
+
+
+class MoveRight(Action):
+    def __init__(self, agent_id: str, distance: Number):
+        super().__init__(agent_id=agent_id)
+        self.distance = distance
+
+    def act(self, actuator: Actuator):
+        actuator.actuate_move_right(self)
+
+
+class MoveUp(Action):
+    def __init__(self, agent_id: str, distance: Number):
+        super().__init__(agent_id=agent_id)
+        self.distance = distance
+
+    def act(self, actuator: Actuator):
+        actuator.actuate_move_up(self)
+
+
+class MoveDown(Action):
+    def __init__(self, agent_id: str, distance: Number):
+        super().__init__(agent_id=agent_id)
+        self.distance = distance
+
+    def act(self, actuator: Actuator):
+        actuator.actuate_move_down(self)
